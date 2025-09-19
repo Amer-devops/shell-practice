96f46d95c649bedb603f302b237bfaa7c8ded207
amer-devops <amerahmed.official@gmail.com>
2025-09-19 17:58:02 +0530

    some message

---
+++ 3-variables.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+PERSON1=NAVEEN
+PERSON2=JAFFAR
+echo "$PERSON1:: Hi $PERSON2 how are you"
+echo "$PERSON2:: hello $PERSON1 I am fine, what about you"
+echo "$PERSON1:: I am also fine, what are doing now"
+echo "$PERSON2:: Doing office work $PERSON1"
