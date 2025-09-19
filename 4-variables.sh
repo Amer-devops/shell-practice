7aa74c46027b9948b08b67a0ceda0f336dc31cbe
amer-devops <amerahmed.official@gmail.com>
2025-09-19 18:09:38 +0530

    some message

---
+++ 4-variables.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "Asaduddin-Owais:: welcome akbar-uddin"
+echo "akbar-uddin:: what matter is going in assembley Asaduddin-Owais"
+echo "Asaduddin-Owais:: In assembley taking about vot chori akbar-uddin"
+echo "akbar-uddin:: wow good what about the discussion then what is the matter Asaduddin-Owais"
