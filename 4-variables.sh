0a345bd17e7493a4facbef3744e92118e9a053a5
amer-devops <amerahmed.official@gmail.com>
2025-09-20 17:14:18 +0530

    some message

--- 4-variables.sh
+++ 4-variables.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
-echo "Asaduddin-Owais:: welcome akbar-uddin"
-echo "akbar-uddin:: what matter is going in assembley Asaduddin-Owais"
-echo "Asaduddin-Owais:: In assembley taking about vot chori akbar-uddin"
-echo "akbar-uddin:: wow good what about the discussion then what is the matter Asaduddin-Owais"
+# Arguments
+PERSON1=$1
+PERSON2=$2
+
+echo "$PERSON1:: Welcome to Alaska $PERSON2"
+echo "$PERSON2:: Thank you $PERSON1, Finally not on Zoom Webinar"
+echo "$PERSON1:: So, do we have a deal"
+echo "$PERSON2:: Yes, if you make Ukraine joins Russia"
--- 5-variables.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-person1=$1
-person2=$2
-echo "$PERSON1:: welcome $PERSON2"
-echo "$PERSON2:: what matter is going in assembley $PERSON1"
-echo "$PERSON1:: In assembley taking about vot chori $PERSON2"
-echo "$PERSON2:: wow good what about the discussion then what is the matter $PERSON1"
