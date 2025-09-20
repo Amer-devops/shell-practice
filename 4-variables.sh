4836ed5f80c51fee9c580ed05294e947088d3e2b
Mohammed Amer Ahmed <123250275+Amer-devops@users.noreply.github.com>
2025-09-20 17:15:17 +0530

    Delete 4-variables.sh

--- 4-variables.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-echo "Asaduddin-Owais:: welcome akbar-uddin"
-echo "akbar-uddin:: what matter is going in assembley Asaduddin-Owais"
-echo "Asaduddin-Owais:: In assembley taking about vot chori akbar-uddin"
-echo "akbar-uddin:: wow good what about the discussion then what is the matter Asaduddin-Owais"
