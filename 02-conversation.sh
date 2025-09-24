3344f1dfe817c086ec0d57ab661a2454d9613d8b
Mohammed Amer Ahmed <123250275+Amer-devops@users.noreply.github.com>
2025-09-24 13:37:21 +0530

    Delete 02-conversation.sh

--- 02-conversation.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-echo "Amer:: hi Irfan, how are you"
-echo "Irfan:: I am fine Amer, how are you"
-echo "Amer:: I am also fine Irfan , what are doing"
-echo "Irfan:: working on dot net services Amer"
