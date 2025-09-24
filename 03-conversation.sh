54149711ae2e74b206978ae16c9cc59ec5758477
amer-devops <amerahmed.official@gmail.com>
2025-09-24 14:22:01 +0530

    conversation

--- 03-conversation.sh
+++ 03-conversation.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-PERSON1="Sachin"
-PERSON2="Rahul"
+PERSON1="Amer"
+PERSON2="Irfan"
 
 echo "$PERSON1:: Hi $PERSON2, How are you?"
 echo "$PERSON2:: Hello $PERSON1, I am fine. How are you?"
