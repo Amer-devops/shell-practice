0c1713949b4b6c7a0d8e9078b0cd05ccb6b15ee4
amer-devops <amerahmed.official@gmail.com>
2025-09-24 18:43:35 +0530

    variable

--- 11-installation.sh
+++ 11-installation.sh
@@ -4,4 +4,14 @@ USERID=$(id -u)
 
 if [ $USERID -ne 0 ]; then
     echo "ERROR:: run the script with root previlage"
+    exit 1
+fi
+
+dnf install mysql-server -y
+
+if [ $? -ne 0 ]; then
+    echo "ERROR:: Installing mysql server is failure"
+    exit 1
+else
+    echo "ERROR:: Installing mysql server is success"
 fi
