2efed597d3da9b486022d3685fdc2aa9efb2eb4c
amer-devops <amerahmed.official@gmail.com>
2025-09-24 19:00:27 +0530

    installation

---
+++ 13-installation.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+USERID=$(id -u)
+
+if [ $USERID -ne 0 ]; then
+   echo "ERROR:: run the script with root privelege"
+   exit 1 
+fi
+
+dnf install Nginx -y
+
+if [ $? -ne 0 ]; then
+   echo "ERROR:: Installing Nginx is failure"
+   exit 1
+else
+   echo "ERROR:: Installing Nginx is success"
+fi
