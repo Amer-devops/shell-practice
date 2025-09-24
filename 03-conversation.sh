4b135b1e7a35d504e94aa9a18767ad183a0465e8
Mohammed Amer Ahmed <123250275+Amer-devops@users.noreply.github.com>
2025-09-24 13:37:30 +0530

    Delete 03-conversation.sh

--- 03-conversation.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-PERSON1=$PERSON1
-PERSON2=$PERSON2
-
-echo "$PERSON1:: hi $PERSON2, how are you"
-echo "$PERSON2:: I am fine $PERSON1, how are you"
-echo "$PERSON1:: I am also fine $PERSON2 , what are doing"
-echo "$PERSON2:: working on dot net services $PERSON1"
