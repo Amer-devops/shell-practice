c0b47b7f5cf39eb1b6ee30d7c12a29b5afe7ecd8
amer-devops <amerahmed.official@gmail.com>
2025-09-24 18:49:08 +0530

    installation

--- 11-installation.sh
+++ 11-installation.sh
@@ -3,11 +3,11 @@
 USERID=$(id -u)
 
 if [ $USERID -ne 0 ]; then
-    echo "ERROR:: run the script with root previlage"
-    exit 1
+    echo "ERROR:: please run the script with root previlage"
+    exit 1 # failure is other than 0
 fi
 
-dnf install mysql-server -y
+dnf install mysql -y
 
 if [ $? -ne 0 ]; then
     echo "ERROR:: Installing Mysql server is failure"
