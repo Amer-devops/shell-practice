ef4ad3dd54af2489f02c39198d29cbb8c9d2bb1c
amer-devops <amerahmed.official@gmail.com>
2025-09-24 20:20:18 +0530

    conversation

--- 17-colors.sh
+++ 17-colors.sh
@@ -14,21 +14,32 @@ fi
 
 VALIDATE(){ # functions receive inputs through args just like shell script args
     if [ $1 -ne 0 ]; then
-    echo -e "ERROR:: Installing $2...$R failure $N"
+    echo -e "ERROR:: Installing $$2 ... $R failure $N"
     exit 1 # failure is other than 0
 
     else
-    echo -e "Installing $2...$G SUCCESS $N"
+    echo -e "Installing $2 ... $G SUCCESS $N"
     fi
 }
 
-dnf install mysql -y
-VALIDATE $? "mysql"
+if [ $? -ne 0 ]; then
+    dnf install mysql -y
+    VALIDATE $? "MySQL"
+else
+    echo -e "MySQL already exist ... $Y SKIPPING $N"
+fi
 
-dnf install nginx -y
-VALIDATE $? "Nginx"
+if [ $? -ne 0 ]; then
+    dnf install nginx -y
+    VALIDATE $? "Nginx"
+else
+    echo -e "Nginx already exist ... $Y SKIPPING $N"
+fi
 
-dnf install python3 -y
-VALIDATE $? "python3"
-
+if [ $? -ne 0 ]; then
+    dnf install python3 -y
+    VALIDATE $? Python3"
+else
+    echo -e "Python3 already exist ... $Y SKIPPING $N"
+fi
 
