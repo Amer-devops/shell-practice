91babc1d45076cf334a1719bee46d2847b4bf313
amer-devops <amerahmed.official@gmail.com>
2025-09-24 14:26:44 +0530

    conversation

---
+++ 04-variable.sh
@@ -0,0 +1,7 @@
+PERSON1=$1
+PERSON2=$2
+
+echo "$1:: Hi $2, How are you?"
+echo "$2:: Hello $1, I am fine. How are you?"
+echo "$1:: What are you learning recently?"
+echo "$2:: I am learning Shell scripting $1"
