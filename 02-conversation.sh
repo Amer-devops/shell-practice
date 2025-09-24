da3d976cac09058fa83fc02ac896988dd8b758b6
amer-devops <amerahmed.official@gmail.com>
2025-09-24 14:23:17 +0530

    conversation

--- 02-conversation.sh
+++ 02-conversation.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo "Ramesh:: Hi Suresh, How are you?"
-echo "Suresh:: Hello Ramesh, I am fine. How are you?"
-echo "Ramesh:: What are you learning recently?"
-echo "Suresh:: I am learning Shell scripting Ramesh"
+echo "Amer:: Hi Irfan, How are you?"
+echo "Irfan:: Hello Amer, I am fine. How are you?"
+echo "Amer:: What are you learning recently?"
+echo "Irfan:: I am learning Shell scripting Amer"
