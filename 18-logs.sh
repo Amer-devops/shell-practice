2b04ffb84a2e6ecb0082bf8108bccfdb8daeff0f
amer-devops <amerahmed.official@gmail.com>
2025-09-25 13:34:29 +0530

    conversation

--- 18-logs.sh
+++ 18-logs.sh
@@ -9,14 +9,9 @@ N="\e[0m"
 LOGS_FOLDER="/var/log/shell-script"
 SCRIPT_NAME=$( echo $0 | cut -d "." -f1 )
 LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log" # /var/log/shell-script/16-logs.log
-TOTAL_TIME=$(($END_TIME-$START_TIME))
-
-echo "Script executed in: $TOTAL_TIME Seconds"
 
 mkdir -p $LOGS_FOLDER
-
-echo "Script started executed at: $(date)" 
-START_TIME=$(date +%s)
+echo "Script started executed at: $(date)" | tee -a $LOG_FILE
 
 if [ $USERID -ne 0 ]; then
     echo "ERROR:: Please run this script with root privelege"
@@ -25,10 +20,10 @@ fi
 
 VALIDATE(){ # functions receive inputs through args just like shell script args
     if [ $1 -ne 0 ]; then
-        echo -e "Installing $2 ... $R FAILURE $N" 
+        echo -e "Installing $2 ... $R FAILURE $N" | tee -a $LOG_FILE
         exit 1
     else
-        echo -e "Installing $2 ... $G SUCCESS $N" 
+        echo -e "Installing $2 ... $G SUCCESS $N" | tee -a $LOG_FILE
     fi
 }
 
@@ -38,14 +33,15 @@ if [ $? -ne 0 ]; then
     dnf install mysql -y &>>$LOG_FILE
     VALIDATE $? "MySQL"
 else
-    echo -e "MySQL already exist ... $Y SKIPPING $N" 
+    echo -e "MySQL already exist ... $Y SKIPPING $N" | tee -a $LOG_FILE
+fi
 
 dnf list installed nginx &>>$LOG_FILE
 if [ $? -ne 0 ]; then
     dnf install nginx -y &>>$LOG_FILE
     VALIDATE $? "Nginx"
 else
-    echo -e "Nginx already exist ... $Y SKIPPING $N" 
+    echo -e "Nginx already exist ... $Y SKIPPING $N" | tee -a $LOG_FILE
 fi
 
 dnf list installed python3 &>>$LOG_FILE
@@ -53,7 +49,5 @@ if [ $? -ne 0 ]; then
     dnf install python3 -y &>>$LOG_FILE
     VALIDATE $? "python3"
 else
-    echo -e "Python3 already exist ... $Y SKIPPING $N" 
+    echo -e "Python3 already exist ... $Y SKIPPING $N" | tee -a $LOG_FILE
 fi
-
-END_TIME=$(date +%s)
