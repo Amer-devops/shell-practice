e9012bc179fdc735b135b2de51be7a17b37854a3
amer-devops <amerahmed.official@gmail.com>
2025-09-25 16:32:55 +0530

    conversation

---
+++ 20-loops.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+USERID=$(id -u)
+
+if [ $USERID -ne 0 ]; then
+   echo "ERROR:: please run the script with root privelege"
+   exit 1 
+fi
+
+VALIDATE(){ # functions receive inputs through args just like shell script args
+    if [ $1 -ne 0 ]; then
+    echo -e "ERROR:: Installing $$2 ... $R failure $N"
+    exit 1 # failure is other than 0
+
+    else
+    echo -e "Installing $2 ... $G SUCCESS $N"
+    fi
+}
+
+for package in $@
+
+do
+
+dnf list installed $package &>>LOG_FILE
+
+if [ $1 -ne 0 ]; then
+dnf install $package -y &>>LOG_FILE
+
+else
+ 
+echo -e "Already installed...$Y SKIPPING $N"
+
+fi
+
+done
