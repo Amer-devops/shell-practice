eac4d80d4ca99c2ae134e9e8d3c9d45594490e5d
amer-devops <amerahmed.official@gmail.com>
2025-09-25 12:58:26 +0530

    conversation

--- 18-logs.sh
+++ 18-logs.sh
@@ -6,11 +6,11 @@ G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
 
-mkdir -p $LOGS_FOLDER
-
 LOGS_FOLDER="/var/log/shell-script"
 SCRIPT_NAME=$( echo $0 | cut -d "." -f1 )
 LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log" # /var/log/shell-script/16-logs.log
+
+mkdir -p $LOGS_FOLDER
 
 echo "Script started executed at: $(date)" 
 
