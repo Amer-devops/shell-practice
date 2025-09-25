c38baa412c5f4e804a6a6e20f0524aaea1a7da12
amer-devops <amerahmed.official@gmail.com>
2025-09-25 12:26:16 +0530

    conversation

--- 18.logs.sh
+++ 18.logs.sh
@@ -8,7 +8,7 @@ N="\e[0m"
 
 mkdir -p $LOG_FILE
 
-LOG_FOLDER= "/var/log/shell-script"
+LOG_FOLDER= "/var/log/shell-practice"
 SCRIPT_NAME=$( echo $0 | cut -d "." -f1 )
 LOG_FILE="$LOG_FOLDER/$SCRIPT_NAME.log"
 
