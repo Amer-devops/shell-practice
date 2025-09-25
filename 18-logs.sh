c81ef0fc3c3ac8c6918dda1d0e3fedefb852e6d6
amer-devops <amerahmed.official@gmail.com>
2025-09-25 13:28:42 +0530

    conversation

--- 18-logs.sh
+++ 18-logs.sh
@@ -10,7 +10,9 @@ LOGS_FOLDER="/var/log/shell-script"
 SCRIPT_NAME=$( echo $0 | cut -d "." -f1 )
 LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log" # /var/log/shell-script/16-logs.log
 TOTAL_TIME=$(($END_TIME-$START_TIME))
-echo "Script executed in :$TOTAL_TIME
+
+echo "Script executed in: $TOTAL_TIME Seconds"
+
 mkdir -p $LOGS_FOLDER
 
 echo "Script started executed at: $(date)" 
