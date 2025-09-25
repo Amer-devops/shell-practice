cbaf3458b418941e6faca707aa4cb07b335fa4ec
amer-devops <amerahmed.official@gmail.com>
2025-09-25 16:56:54 +0530

    conversation

--- 20-loops.sh
+++ 20-loops.sh
@@ -11,7 +11,7 @@ SCRIPT_NAME=$( echo $0 | cut -d "." -f1 )
 LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log" # /var/log/shell-script/16-logs.log
 
 mkdir -p $LOGS_FOLDER
-echo "Script started executed at: $(date)" | tee -a $LOG_FILE
+echo -e"Script started started at: $R $($date) $N" | tee -a $LOG_FILE
 
 START_TIME=$(date +%s)
 
